2b606851c41f25c1b85a77900e05cdca78e34114
Adam Stefaniak <a.stefaniak900@gmail.com>
2020-11-16 19:57:13 +0100

    added sign in and registration to backend

---
+++ backend/data.js
@@ -0,0 +1,19 @@
+import bcrypt from "bcryptjs";
+
+const data = {
+  users: [
+    {
+      name: "Basir",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+    },
+    {
+      name: "John",
+      email: "user@example.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: false,
+    },
+  ],
+};
+export default data;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,32 @@
-const test = 4;
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routers/userRouter.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/gym", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+app.use("/users", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
