635e094bff2affa8be56800e0f71bc7a66c99221
Adam Stefaniak <a.stefaniak900@gmail.com>
2020-11-26 17:56:58 +0100

    update

--- utils.js
+++ utils.js
@@ -22,3 +22,16 @@ module.exports = generateToken;
 // "start": "concurrently \"npm run server\" \"npm run client\""
 // "start": "node server.js"
 //
+
+// "scripts": {
+//   "test": "echo \"Error: no test specified\" && exit 1",
+//   "build": "cd client && npm run build",
+//   "install-client": "npm install --prefix client",
+//   "heroku-postbuild": "cd client && npm install --only=dev && npm install && npm run build",
+//   "predeploy": "npm run build",
+//   "deploy": "gh-pages -d build",
+//   "server": "nodemon server.js",
+//   "client": "npm run start --prefix client",
+//   "dev": "concurrently \"npm run server\" \"npm run client\"",
+//   "start": "concurrently \"node server.js\" \"npm run start --prefix client\""
+// },
