8cda181f9b899911f772e2f313754f698c362a9f
Adam Stefaniak <a.stefaniak900@gmail.com>
2020-11-26 11:12:47 +0100

    first commit

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  });
 }
 
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/gym", {
