95ea192d94f17e44269d39a25a2cb8700e16cc00
Adam Stefaniak <a.stefaniak900@gmail.com>
2020-11-26 18:03:52 +0100

    update

---
+++ client/build/precache-manifest.98dbef470c64404b143b35d3ce67a3f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125351712a026e33021eaf7805e68d6b",
+    "url": "/gym/index.html"
+  },
+  {
+    "revision": "1e72f4ed4342ad83e874",
+    "url": "/gym/static/css/main.457a00d2.chunk.css"
+  },
+  {
+    "revision": "74bc2dd40c0dcd55d7d7",
+    "url": "/gym/static/js/2.f4244055.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gym/static/js/2.f4244055.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e72f4ed4342ad83e874",
+    "url": "/gym/static/js/main.15615c7e.chunk.js"
+  },
+  {
+    "revision": "91978e2885927efd4543",
+    "url": "/gym/static/js/runtime-main.fdf1fd59.js"
+  },
+  {
+    "revision": "7a25fd0a5ae096b4104c55807bc360c4",
+    "url": "/gym/static/media/hamburgerS.7a25fd0a.svg"
+  },
+  {
+    "revision": "8f6a946126ab7104eec689d4db78314d",
+    "url": "/gym/static/media/stopwatch.8f6a9461.svg"
+  }
+]);
